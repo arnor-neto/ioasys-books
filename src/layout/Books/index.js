010d5397d5f78b96d73bf2dfcd4691afa51fdad1
Arnor Neto <arnorneto10@gmail.com>
2021-04-08 18:25:31 -0300

    redirect correction

--- src/layout/Books/index.js
+++ src/layout/Books/index.js
@@ -5,9 +5,11 @@ import UserMenu from "../../components/UserMenu";
 import PageNavigation from "../../components/PageNavigation";
 import { Context } from "../../GlobalContext";
 import { useEffect, useState, useContext } from "react";
+import { useHistory } from "react-router-dom";
 import axios from "axios";
 
 const Books = () => {
+  let history = useHistory();
   const [user] = useContext(Context);
   const [books, setBooks] = useState([]);
   const [totalPages, setTotalPages] = useState(1);
@@ -44,26 +46,30 @@ const Books = () => {
   };
 
   useEffect(() => {
-    setLoading(true);
-    axios
-      .get(
-        `https://books.ioasys.com.br/api/v1/books?page=${currentPage}&amount=12`,
-        {
-          headers: {
-            Authorization: "Bearer " + user.auth,
-          },
-        }
-      )
-      .then((response) => {
-        setBooks(response.data.data);
-        setTotalPages(Math.ceil(response.data.totalPages));
-        setCurrentPage(response.data.page);
-        console.log(response.data);
-        setLoading(false);
-      })
-      .catch((error) => {
-        console.log("Erro na requisição");
-      });
+    if (user.auth === "null") {
+      history.push("/");
+    } else {
+      setLoading(true);
+      axios
+        .get(
+          `https://books.ioasys.com.br/api/v1/books?page=${currentPage}&amount=12`,
+          {
+            headers: {
+              Authorization: "Bearer " + user.auth,
+            },
+          }
+        )
+        .then((response) => {
+          setBooks(response.data.data);
+          setTotalPages(Math.ceil(response.data.totalPages));
+          setCurrentPage(response.data.page);
+          console.log(response.data);
+          setLoading(false);
+        })
+        .catch((error) => {
+          console.log("Erro na requisição");
+        });
+    }
   }, [currentPage, user.auth]);
 
   return (
